e637ce15fd33976b236b0840f7cc6f56dd8d2dd1
ksrandive <ksrandive@gmail.com>
2021-03-30 13:12:17 +0530

    jdk arrays

---
+++ src/main/java/com/ks/learning/sortingArrays/SortingArrays.java
@@ -0,0 +1,12 @@
+package com.ks.learning.sortingArrays;
+
+import java.util.Arrays;
+
+public class SortingArrays {
+
+    public static void main(String[] args) {
+        int[] intArray = {80, 12, -10, 1, 15, -20, 18, -55, 55, -1};
+        Arrays.sort(intArray);
+        System.out.println(Arrays.toString(intArray));
+    }
+}
